d056b008502cce559d8b37b6be9edb134eab9845
D4707 <kimhyun@gsretail.com>
2022-05-11 11:33:45 +0900

    first commit

---
+++ src/test/java/eblo/study/springboot/StudySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package eblo.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
