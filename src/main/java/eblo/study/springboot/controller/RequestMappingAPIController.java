cf304576934bd0cb75673c6de27881bf96078f18
kkaok <jack1972@naver.com>
2022-05-11 19:02:28 +0900

    comments 추가

--- src/main/java/eblo/study/springboot/controller/RequestMappingAPIController.java
+++ src/main/java/eblo/study/springboot/controller/RequestMappingAPIController.java
@@ -12,31 +12,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/controller/mapping")
 public class RequestMappingAPIController {
 
+    /**
+     * 모든 메소드 가능
+     * @return
+     */
     @RequestMapping("/requestMapping")
     public ResponseEntity<String> requestMapping() {
         return ResponseEntity.ok("requestMapping");
     }
     
+    /**
+     * @RequestMapping(method = RequestMethod.GET)와 동일  
+     * @return
+     */
     @GetMapping
     public ResponseEntity<String> getMapping() {
         return ResponseEntity.ok("getMapping");
     }
     
+    /**
+     * @RequestMapping(method = RequestMethod.POST)와 동일  
+     * @return
+     */
     @PostMapping
     public ResponseEntity<String> postMapping() {
         return ResponseEntity.ok("postMapping");
     }
 
+    /**
+     * @RequestMapping(method = RequestMethod.PUT)와 동일  
+     * @return
+     */
     @PutMapping
     public ResponseEntity<String> putMapping() {
         return ResponseEntity.ok("putMapping");
     }
 
+    /**
+     * @RequestMapping(method = RequestMethod.DELETE)와 동일  
+     * @return
+     */
     @DeleteMapping
     public ResponseEntity<String> deleteMapping() {
         return ResponseEntity.ok("deleteMapping");
     }
 
+    /**
+     * @RequestMapping(method = RequestMethod.GET, path="/test1")와 동일  
+     * @return
+     */
     @GetMapping("/test1")
     public ResponseEntity<String> getMappingTest() {
         return ResponseEntity.ok("getMapping-test1");
