16dd294d5cbd5792b91575626355d5bf0d7fcead
까오기 <jack1972@naver.com>
2022-05-19 19:30:02 +0900

    logback 설정 추가

--- src/main/java/eblo/study/springboot/profile/ProfileTestAPIController.java
+++ src/main/java/eblo/study/springboot/profile/ProfileTestAPIController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class ProfileTestAPIController {
 
@@ -12,6 +15,9 @@ public class ProfileTestAPIController {
     
     @GetMapping("/profile")
     public String test() {
+        log.debug("debug : "+appTitle);
+        log.info("info : "+appTitle);
+        log.error("error : "+appTitle);
         return appTitle;
     }
     
